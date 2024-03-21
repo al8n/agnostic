44b781a008d478718fba939e02d70cc7086d179a
Al Liu <scygliu1@gmail.com>
2024-03-21 11:04:59 +0800

    Fix testcases for tokio delay

--- lite/src/tokio/delay.rs
+++ lite/src/tokio/delay.rs
@@ -19,90 +19,80 @@ mod tests {
   const RESET: Duration = Duration::from_millis(2);
   const BOUND: Duration = Duration::from_millis(50);
 
-  #[test]
-  fn test_delay() {
-    futures::executor::block_on(async {
-      let start = Instant::now();
-      let ctr = Arc::new(AtomicUsize::new(0));
-      let ctr1 = ctr.clone();
-      let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
-        ctr1.fetch_add(1, Ordering::SeqCst);
-      });
-      delay.await.unwrap();
-      let elapsed = start.elapsed();
-      assert!(elapsed >= DELAY);
-      assert!(elapsed < DELAY + BOUND);
-      assert_eq!(ctr.load(Ordering::SeqCst), 1);
+  #[tokio::test]
+  async fn test_delay() {
+    let start = Instant::now();
+    let ctr = Arc::new(AtomicUsize::new(0));
+    let ctr1 = ctr.clone();
+    let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
+      ctr1.fetch_add(1, Ordering::SeqCst);
     });
+    delay.await.unwrap();
+    let elapsed = start.elapsed();
+    assert!(elapsed >= DELAY);
+    assert!(elapsed < DELAY + BOUND);
+    assert_eq!(ctr.load(Ordering::SeqCst), 1);
   }
 
-  #[test]
-  fn test_delay_at() {
-    futures::executor::block_on(async {
-      let start = Instant::now();
-      let ctr = Arc::new(AtomicUsize::new(0));
-      let ctr1 = ctr.clone();
-      let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay_at(start + DELAY, async move {
-        ctr1.fetch_add(1, Ordering::SeqCst);
-      });
-      delay.await.unwrap();
-      let elapsed = start.elapsed();
-      assert!(elapsed >= DELAY);
-      assert!(elapsed < DELAY + BOUND);
-      assert_eq!(ctr.load(Ordering::SeqCst), 1);
+  #[tokio::test]
+  async fn test_delay_at() {
+    let start = Instant::now();
+    let ctr = Arc::new(AtomicUsize::new(0));
+    let ctr1 = ctr.clone();
+    let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay_at(start + DELAY, async move {
+      ctr1.fetch_add(1, Ordering::SeqCst);
     });
+    delay.await.unwrap();
+    let elapsed = start.elapsed();
+    assert!(elapsed >= DELAY);
+    assert!(elapsed < DELAY + BOUND);
+    assert_eq!(ctr.load(Ordering::SeqCst), 1);
   }
 
-  #[test]
-  fn test_delay_reset() {
-    futures::executor::block_on(async {
-      let start = Instant::now();
-      let ctr = Arc::new(AtomicUsize::new(0));
-      let ctr1 = ctr.clone();
-      let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
-        ctr1.fetch_add(1, Ordering::SeqCst);
-      });
-      futures_util::pin_mut!(delay);
-      AsyncDelay::reset(delay.as_mut(), RESET);
-      delay.await.unwrap();
-      let elapsed = start.elapsed();
-      assert!(elapsed >= RESET);
-      assert!(elapsed < RESET + BOUND);
-      assert_eq!(ctr.load(Ordering::SeqCst), 1);
+  #[tokio::test]
+  async fn test_delay_reset() {
+    let start = Instant::now();
+    let ctr = Arc::new(AtomicUsize::new(0));
+    let ctr1 = ctr.clone();
+    let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
+      ctr1.fetch_add(1, Ordering::SeqCst);
     });
+    futures_util::pin_mut!(delay);
+    AsyncDelay::reset(delay.as_mut(), RESET);
+    delay.await.unwrap();
+    let elapsed = start.elapsed();
+    assert!(elapsed >= RESET);
+    assert!(elapsed < RESET + BOUND);
+    assert_eq!(ctr.load(Ordering::SeqCst), 1);
   }
 
-  #[test]
-  fn test_delay_abort() {
-    futures::executor::block_on(async {
-      let start = Instant::now();
-      let ctr = Arc::new(AtomicUsize::new(0));
-      let ctr1 = ctr.clone();
-      let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
-        ctr1.fetch_add(1, Ordering::SeqCst);
-      });
-      AsyncDelay::abort(&delay);
-      assert!(delay.await.is_err());
-      let elapsed = start.elapsed();
-      assert!(elapsed < DELAY);
-      assert_eq!(ctr.load(Ordering::SeqCst), 0);
+  #[tokio::test]
+  async fn test_delay_abort() {
+    let start = Instant::now();
+    let ctr = Arc::new(AtomicUsize::new(0));
+    let ctr1 = ctr.clone();
+    let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
+      ctr1.fetch_add(1, Ordering::SeqCst);
     });
+    AsyncDelay::abort(&delay);
+    assert!(delay.await.is_err());
+    let elapsed = start.elapsed();
+    assert!(elapsed < DELAY);
+    assert_eq!(ctr.load(Ordering::SeqCst), 0);
   }
 
-  #[test]
-  fn test_delay_cancel() {
-    futures::executor::block_on(async {
-      let start = Instant::now();
-      let ctr = Arc::new(AtomicUsize::new(0));
-      let ctr1 = ctr.clone();
-      let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
-        ctr1.fetch_add(1, Ordering::SeqCst);
-      });
-      AsyncDelay::cancel(&delay);
-      assert!(delay.await.is_ok());
-      let elapsed = start.elapsed();
-      assert!(elapsed < BOUND);
-      assert_eq!(ctr.load(Ordering::SeqCst), 1);
+  #[tokio::test]
+  async fn test_delay_cancel() {
+    let start = Instant::now();
+    let ctr = Arc::new(AtomicUsize::new(0));
+    let ctr1 = ctr.clone();
+    let delay = <TokioDelay<_> as AsyncDelayExt<_>>::delay(DELAY, async move {
+      ctr1.fetch_add(1, Ordering::SeqCst);
     });
+    AsyncDelay::cancel(&delay);
+    assert!(delay.await.is_ok());
+    let elapsed = start.elapsed();
+    assert!(elapsed < BOUND);
+    assert_eq!(ctr.load(Ordering::SeqCst), 1);
   }
 }
